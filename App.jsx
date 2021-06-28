83d61a1705ce8564ea317548a51cf6458a5a9ee5
SidduSiddardha <86155350+SidduSiddardha@users.noreply.github.com>
2021-06-28 18:10:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./header"
+import Footer from "./footer"
+import Info from "./info"
+function App(){
+  return(
+<div>
+     <Header />
+     <Info />
+     <Info />
+     <Info />
+     <Info />
+     <Info />
+     <Footer />
+     </div> 
+  );
+}
+
+export default App;
---
+++ footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+  return(
+    <footer><p>Copyright by ShapeAi @ {new Date().getFullYear()}</p></footer>
+  );
+}
+
+export default Footer;
